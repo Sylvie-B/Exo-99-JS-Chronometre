b77ea0c55433456b3eb451fcde63b0112658430c
Sylvie-B <unsolo@hotmail.fr>
2020-12-23 11:29:09 +0100

    unstopable

--- exercice1.js
+++ exercice1.js
@@ -1,12 +1,20 @@
 // Make it count up on a timer, calling this function
 let seconds = document.getElementById("seconds");
-let countUp = function() {
-    // ?
-};
+let t = 0;
+let stopButton = document.getElementById("stop-button");
 
 // How can you make it stop counting?
 let stopCountUp = function() {
+    clearTimeout(countUp);
 };
-let stopButton = document.getElementById("stop-button");
-stopButton.addEventListener("click", stopCountUp);
 
+let countUp = function() {
+    setTimeout(function (){
+        seconds.innerHTML = t.toString();
+        t++;
+        countUp();
+        }, 1000);
+    stopButton.addEventListener("click", stopCountUp);
+};
+
+countUp();
